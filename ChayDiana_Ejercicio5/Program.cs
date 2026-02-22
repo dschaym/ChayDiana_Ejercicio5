1062044ae1040e53f9ffd047f41c862b3cc0277d
dschaym <dschay@gmail.com>
2026-02-22 17:23:26 -0600

    Creacion de proyecto

--- ChayDiana_Ejercicio5/Program.cs
+++ ChayDiana_Ejercicio5/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Console.WriteLine("EVALUACION DE PRESTAMO BANCARIO POR REGLAS");
+Console.WriteLine("- Tipo de solicitante -");
+Console.WriteLine("1. Empleado fijo");
+Console.WriteLine("2. Empleado temporal");
+Console.WriteLine("3. Trabajador independiente");
+Console.WriteLine("4. Estudiante");
+Console.Write("Ingrese el número correspondiente al tipo de solicitante (1-4): ");
+int tipoSolicitante = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingreso mensual (con punto decimal ej. 5223.12) ");
+double ingresoMensual = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Antigüedad laboral en meses (meses)");
+int antiguedadmesual = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Monto solicitado:");
+double monto = double.Parse(Console.ReadLine());
+
+Console.WriteLine("- Historial crediticio -");
+Console.WriteLine("1. Excelente");
+Console.WriteLine("2. Bueno");
+Console.WriteLine("3. Regular");
+Console.WriteLine("4. Malo");
+Console.Write("Ingrese el número correspondiente al historial crediticio (1-4): ");
+int historialCrediticio = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Tiene fiador? (S/N)");
+string fiador = Console.ReadLine().ToUpper();
+
